9bcc6b90c1a9778bf87e05c355954c4eb1086e62
Amir Rasool <amir1994@gmail.com>
2022-09-07 22:06:52 -0500

    test

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-/* target the button and create an api*/ 
+/*  see the name 
+    see the image 
+    see the type
+    moves - abilities 
 
-/* try and use asycn and await */ 
+*/
+    
 
-/* discuss what we want to do / what we want to display onto our DOM */ 
+document.getElementById("button").addEventListener("click", getfetch)
+// Capitalize Function
+function capitalizeName(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
-/* How should the work be divided ?  */ 
+function getfetch(){
+    const userInput = document.querySelector('input').value.toLowerCase()
+    const url = `https://pokeapi.co/api/v2/pokemon/${userInput}`
 
-/* optimize code */ 
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        //setting const variable equal to grabbed API data
+        const name = data.name
+        // slice(startIndex, endIndex) // slice(0,3) Dit does not include the last  
+        // slice(1) itto
+        // D + itto
+        const type = data.types[0].type.name  
+        const abilities = data.abilities
+        const image = data.sprites.other["official-artwork"]["front_default"]    
 
-/* Push? add carousel */ 
+        // Display On the DOM
+        document.getElementById('name').innerText = `Name: ${capitalizeName(name)}`
+        document.getElementById('type').innerText = `Type: ${capitalizeName(type)}`
+        document.getElementById('abilities').innerText = `Abilities: ${capitalizeName(abilities)}`
+        document.querySelector('img').src = image
+
+    })
+}
+
